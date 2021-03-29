bd15ae42867621d8a24cc694510275e4c2e29a28
Elijah Best <57592789+ElijahhDev@users.noreply.github.com>
2021-03-29 12:10:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const Discord = require('discord.js')
 const client = new Discord.Client({presence: {
     status: 'idle'}})
-const { token } = require('./config.json');
+    client.login(token) // login to discord
+const config = require('./config.json')
 const Command = require('./handlers/Command');
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 ["command", "events"].forEach(handler => {
     require(`./handlers/${handler}`)(client);
 });
+client.config = config.bot
 client.emotes = require('./structures/Emotes.json')
 client.config = require('./config.json')
 client.gif = 'https://cdn.discordapp.com/attachments/824757489051369492/824769051364294706/MutualBot_Logo.gif'
 client.color = '#5693f5'
 client.say = require('./structures/customEmbeds.js').embed
-client.login(token)
